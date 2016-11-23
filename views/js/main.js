deaf76c6c9bd3edd0544092edd00007519e229c8
arulk8 <arulmurugan194@gmail.com>
2016-11-23 17:40:09 +0530

    splitted the data

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < document.getElementsByClassName("randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
+      var newwidth = (document.getElementsByClassName("randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -501,9 +501,20 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  
+
+  var items = document.getElementsByClassName('mover');
+
+  var phasearry=[];
+  for(var i=0 ;i<5;i++)
+  {
+  phasearry[i]= Math.sin((document.body.scrollTop / 1250)+ i);
+  }
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+
+    var phase = phasearry[i % 5];
+
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
